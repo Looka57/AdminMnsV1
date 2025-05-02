b4e3153e00fcec9e6521c26ed68f65fcf91b2ced
Amandine <napo322@gmail.com>
2025-05-02 15:02:14 +0200

    Mise en place du footer

---
+++ AdminMnsV1/Controllers/FooterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminMnsV1.Controllers
+{
+    public class FooterController : Controller
+    {
+        public IActionResult Footer()
+        {
+            return View();
+        }
+    }
+}
