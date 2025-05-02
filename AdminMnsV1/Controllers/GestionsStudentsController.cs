3e5aa420e48fb509c0d44075c48fdd477d7f77b8
Amandine <napo322@gmail.com>
2025-05-02 14:30:49 +0200

    fichier gestion student + lien des boutons

---
+++ AdminMnsV1/Controllers/GestionsStudentsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminMnsV1.Controllers
+{
+    public class GestionsStudentsController : Controller
+    {
+        public IActionResult GestionStudent()
+        {
+            return View("~/Views/Profils/GestionStudent.cshtml");
+        }
+    }
+}
