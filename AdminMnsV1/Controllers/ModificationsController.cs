420544a7f64646aac1f0bb9c634d81bc935ea191
Amandine <napo322@gmail.com>
2025-05-02 14:42:11 +0200

    fichier modification + mdp + liens

---
+++ AdminMnsV1/Controllers/ModificationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminMnsV1.Controllers
+{
+    public class ModificationsController : Controller
+    {
+        public IActionResult Modification()
+        {
+            return View("~/Views/Profils/Modification.cshtml");
+        }
+    }
+}
