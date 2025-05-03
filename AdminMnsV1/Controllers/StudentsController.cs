8129859b0f1fde2637a939b0ce8e2dfebdd645fc
Amandine <napo322@gmail.com>
2025-05-03 18:24:58 +0200

    fusion des branches

--- AdminMnsV1/Controllers/StudentsController.cs
+++ AdminMnsV1/Controllers/StudentsController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using AdminMnsV1.Models;
+using AdminMnsV1.Data;
 
 namespace AdminMnsV1.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public StudentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Student()
         {
-            return View();
+            // Récupère tous les utilisateurs qui sont de type Student
+            var students = _context.Users
+                .OfType<Student>() 
+                .ToList();
+
+            // Passe la liste des stagiaires à la vue nommée "Student"
+            return View(students);
         }
     }
 }
