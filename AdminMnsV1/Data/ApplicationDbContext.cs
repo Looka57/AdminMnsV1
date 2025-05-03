dff205352941e799d022ceb9ab9846485f362480
Amandine <napo322@gmail.com>
2025-05-03 16:17:08 +0200

    Mise en place des tables Student-User-Admin-Expert avec la hierachie + migration a la bdd

--- AdminMnsV1/Data/ApplicationDbContext.cs
+++ AdminMnsV1/Data/ApplicationDbContext.cs
@@ -12,5 +12,23 @@ namespace AdminMnsV1.Data
         }
 
         public DbSet<Class> Classs { get; set; } // Représente la table "Class"
+        public DbSet<User> Users { get; set; } // Représente la table "User"
+        public DbSet<Expert> Experts { get; set; } // Représente la table "expert"
+        public DbSet<Student> Students { get; set; } // Représente la table "Studnts"
+        public DbSet<Administrator> Administrators { get; set; } // Représente la table "Administrator"
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasDiscriminator<string>("Discriminator") //Indique à EF Core que nous utilisons une colonne nommée "Discriminator" pour distinguer les types dans la hiérarchie User.
+                .HasValue<User>("User")
+                .HasValue<Expert>("Expert_")
+                .HasValue<Student>("Student")
+                .HasValue<Administrator>("Administrator");
+            //Définit les valeurs que prendra la colonne "Discriminator" pour chaque type concret dans la hiérarchie.
+
+        }
     }
 }
---
+++ AdminMnsV1/Models/User.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Net;
+namespace AdminMnsV1.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UserId { get; set; }
+
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string Sexe { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Email { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string PasswordHash { get; set; }
+
+        [MaxLength(150)]
+        public string Address { get; set; }
+
+        [MaxLength(50)]
+        public string City { get; set; }
+
+        public DateTime CreationDate { get; set; }
+        [MaxLength(50)]
+        public string Phone { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        // Propriété de discrimination pour EF Core
+        public string Discriminator { get; set; }
+    }
+}
+
