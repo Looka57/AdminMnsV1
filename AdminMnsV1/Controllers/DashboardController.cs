f092ba80d4788f484b57837c703d909c3cac1900
Amandine <napo322@gmail.com>
2025-05-11 15:53:11 +0200

    mise en place du graphique dynamique stagiaire homme et femme

--- AdminMnsV1/Controllers/DashboardController.cs
+++ AdminMnsV1/Controllers/DashboardController.cs
@@ -15,12 +15,27 @@ namespace AdminMnsV1.Controllers
         }
 
 
-        // Récupère le nombre total d'étudiants
         public IActionResult Dashboard()
         {
+
+        // Récupère le nombre total d'étudiants
             var studentCount = _context.Users
                 .Where(u => (u.Status == "Stagiaire") && !u.IsDeleted)
                 .Count();
+
+            //Recupere le nombre d'hommes'
+
+            var numberMen = _context.Users
+                .Where(u => (u.Status == "Stagiaire") && u.Sexe == "Male")
+                .Count();
+
+            //Recupere le nombre de femmes
+            var numberWomen = _context.Users
+                .Where(u => (u.Status == "Stagiaire") && u.Sexe == "Female")
+                .Count();
+
+
+
             var cards = new List<CardModel>
             {
                 new CardModel { Url = "../Classes/Class", Number = "15", Title = "Classes", IconUrl = "https://img.icons8.com/glyph-neue/64/classroom.png", AltText = "classroom" },
@@ -28,6 +43,10 @@ namespace AdminMnsV1.Controllers
                 new CardModel { Url = "../Students/Student", Number = studentCount.ToString(), Title = "Stagiaires", IconUrl = "https://img.icons8.com/glyph-neue/64/student-male.png", AltText = "student-male" },
                 new CardModel { Url = "#", Number = "5", Title = "Notifications", IconUrl = "https://img.icons8.com/ios-filled/50/appointment-reminders--v1.png", AltText = "reminder" }
             };
+
+            ViewBag.NombreHommes = numberMen;
+            ViewBag.NombreFemmes = numberWomen; // Passe la valeur pour les femmes graphique
+
 
 
             return View(cards);
